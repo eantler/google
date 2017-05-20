957ea30738ade82d1add84cc0dc7a86946820de7
Maya Ekron <mayaekron@Mayas-MacBook-Air.local>
2017-05-20 12:44:31 +0300

    updating results json

--- client/modules/App/AppActions.js
+++ client/modules/App/AppActions.js
@@ -16,17 +16,29 @@ export const SEARCH_DONE = 'SEARCH_DONE';
 const defaultResults = [
           {
             uniqueID: cuid(),
-            name: 'Babies, Fruits',
+            name: 'Kid, Food',
+            memes: [
+              'http://mommyshorts.com/wp-content/uploads/2014/09/6a0133f30ae399970b01b8d0689f9d970c-800wi.jpg'
+              ,'http://m.memegen.com/t8ot6h.jpg'
+              ,'http://images5.aplus.com/uc-up/ba24a3b8-fc7e-40e6-9d37-379b5471fb47/6a0133f30ae399970b01a73e08e2cf970d800wi.resize_640x.jpg'
+              ,'https://i.imgflip.com/n3hlr.jpg'
+              ,'https://img.memesuper.com/69b29cce38f32a64874db8b8eca6d3fc_-kid-meme-eating-food-meme-eating-food_400-400.jpeg'
+            ]
+          },
+          {
+            uniqueID: cuid(),
+            name: 'Baby, Fruit',
             memes: [
               'http://s2.quickmeme.com/img/51/516fbac7acc1f675ba4c3222eba9c6535835a3aed60c0960bb49166f0b1984dd.jpg'
               ,'https://cdn.meme.am/cache/instances/folder438/500x/53576438.jpg'
               ,'https://s-media-cache-ak0.pinimg.com/736x/8b/4c/25/8b4c252e7721751ab69af2bb60f78392--eating-watermelon-watermelon-baby.jpg'
               ,'https://i.imgflip.com/g84ib.jpg'
+              ,'https://i.imgflip.com/hqppv.jpg'
             ]
           },
           {
             uniqueID: cuid(),
-            name: 'Babies, Cakes',
+            name: 'Baby, Cake',
             memes: [
               'https://s-media-cache-ak0.pinimg.com/736x/f6/ba/ef/f6baefaeba0a1a28aebe40876dac37c4.jpg'
               ,'http://www.quickmeme.com/img/18/18b384585e013aa63ea6fc32f344c1417cca02903653621f22711192e16ad8d6.jpg'
@@ -36,22 +48,13 @@ const defaultResults = [
           },
           {
             uniqueID: cuid(),
-            name: 'Kids, Food',
-            memes: [
-              'http://mommyshorts.com/wp-content/uploads/2014/09/6a0133f30ae399970b01b8d0689f9d970c-800wi.jpg'
-              ,'http://m.memegen.com/t8ot6h.jpg'
-              ,'http://images5.aplus.com/uc-up/ba24a3b8-fc7e-40e6-9d37-379b5471fb47/6a0133f30ae399970b01a73e08e2cf970d800wi.resize_640x.jpg'
-              ,'https://i.imgflip.com/n3hlr.jpg'
-            ]
-          },
-          {
-            uniqueID: cuid(),
-            name: 'Mothers, Food',
+            name: 'Mothers, Junk Food',
             memes: [
               'https://s-media-cache-ak0.pinimg.com/736x/20/3e/b9/203eb9ddb040d619d34f85c46202563f.jpg'
               ,'https://static.plive.co.ke/img/incoming/origs6512796/4926367665-w644-h960/16.jpg'
-              ,'http://images.memes.com/meme/914781'
-              ,'https://i.imgflip.com/1899h5.jpg'
+              ,'http://memeguy.com/photos/images/i-call-her-confession-mom-38941.png'
+              ,'http://cdn.ohmygodfacts.com/wp-content/uploads/2015/02/moms-logic-1.jpg'
+              ,'https://pics.onsizzle.com/iwouldneverpoisonimy-children-with-junk-food-that-seemslike-an-inefficient-way-2696713.png'
             ]
           },
         ];
